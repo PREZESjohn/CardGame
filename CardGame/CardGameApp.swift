b4dc4265e77aed620c207803cc86dfe44fa912f9
PREZESs <prezes.john@gmail.com>
2023-10-17 11:37:23 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
