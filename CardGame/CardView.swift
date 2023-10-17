4bdd8cb6bb2934ded5a6ccdf17d13c31e17f2ae5
PREZESs <prezes.john@gmail.com>
2023-10-17 12:28:11 +0200

    add wisuality and function

---
+++ CardGame/CardView.swift
@@ -0,0 +1,31 @@
+//
+//  CardView.swift
+//  CardGame
+//
+//  Created by student on 17/10/2023.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp : Bool = true
+    var body: some View {
+        ZStack{
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(Color.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        //.foregroundColor(.orange)
+        .onTapGesture(perform: {isFaceUp.toggle()})
+    }
+}
+
+#Preview {
+    CardView(content: "String")
+}
--- CardGame/ContentView.swift
+++ CardGame/ContentView.swift
@@ -8,15 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emoijs = ["😀","😃","😆","😅","🤣","😊","🙂"]
+    @State var cardCount = 4
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            ScrollView{
+                cards
+            }
+            cardsCountAdjuster
         }
+        
         .padding()
     }
+    
+    func adjustCardNumber(by offset: Int, symbol: String) -> some View{
+        Button(action:{cardCount += offset}) {
+    
+            Text(symbol)
+                .padding(.horizontal)
+                .font(.largeTitle)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 12).stroke(Color.blue, lineWidth: 2))
+            
+        }
+        
+
+        //.disabled(cardCount<=2 || cardCount>emoijs.count)
+    }
+    
+    var cardsCountAdjuster: some View {
+        HStack {
+            cardRemover
+            Spacer()
+            cardAdder
+
+        }
+    }
+    
+    var cards : some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self){
+                index in
+                CardView(content: emoijs[index])
+                //.aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.blue)
+    }
+    var cardAdder: some View {
+        adjustCardNumber(by: 2, symbol: "+").disabled( cardCount+2>emoijs.count)
+    }
+    var cardRemover: some View{
+        adjustCardNumber(by: -2, symbol: "-").disabled(cardCount<=2)
+    }
+//    struct CardView: View {
+//        let content: String
+//        @State var isFaceUp : Bool = true
+//        var body: some View {
+//            ZStack{
+//                let base = RoundedRectangle(cornerRadius: 12)
+//                if isFaceUp {
+//                    base.fill(Color.white)
+//                    base.strokeBorder(lineWidth: 3)
+//                    Text(content).font(.largeTitle)
+//                } else {
+//                    base.fill()
+//                }
+//            }
+//            .foregroundColor(.orange)
+//            .onTapGesture(perform: {isFaceUp.toggle()})
+//        }
+//    }
+    
 }
 
 #Preview {
